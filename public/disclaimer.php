3202c0ce45033e9db88f677b8f5f31cf90dbc11c
OBXPS\Avinash Barik <avinashbarik91@gmail.com>
2019-07-20 13:03:38 +1000

    Add disclaimer

---
+++ public/disclaimer.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144172971-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-144172971-1');
+		</script>	
+		<title>Disclaimer | Compario - Simple head-to-head comparison for cricket players</title>
+		<meta charset="UTF-8">
+		<meta http-equip="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Simple head-to-head comparison for cricket players">
+		<meta http-equiv="x-ua-compatible" content="ie-edge">
+		<meta property='og:title' content='Disclaimer Policy | Compario'/>		
+		<meta property='og:description' content='Disclaimer | Compario - Simple head-to-head comparison for cricket players'/>
+		<meta property='og:url' content='https://compario.dev'/>
+		<meta property='og:type' content='Open Source'/>
+		<link rel="icon" type="image/png" sizes="96x96" href="/static/favicon-3-96x96.png">
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Righteous&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+		<link rel="stylesheet" href="stylesheet/bootstrap.min.css">
+		<link rel="stylesheet" href="stylesheet/common.css">
+		<script src="js/jquery.slim.min.js"></script>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/common.js"></script>
+		<script src="js/popper.js"></script>		
+		<script src="js/bootstrap.min.js"></script>			
+	</head>
+	<body>
+			<?php include('header.inc'); ?>	
+
+			<div class='container px-5 additional-wrapper'>
+				<div class='row'>
+					<div class='offset-md-2 col-md-8 p-5'>
+						<h1 class='mb-4'><i class="fas fa-user-shield"></i>Disclaimer</h1>
+						<p>Last updated: July 19, 2019.</p>
+						<p class='mb-5'>If you require any more information or have any questions about our site's disclaimer, please feel free to contact us by email at avinashbarik91@gmail.com</p>
+
+						<p class='mb-5'>All the information on this website - https://www.compario.dev/ - is published in good faith and for general information purpose only. Compario does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (Compario), is strictly at your own risk. Compario will not be liable for any losses and/or damages in connection with the use of our website.</p>
+
+						<p class='mb-5'>From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone 'bad'.</p>
+
+						<p class='mb-5'>Please be also aware that when you leave our website, other sites may have different privacy policies and terms which are beyond our control. Please be sure to check the Privacy Policies of these sites as well as their "Terms of Service" before engaging in any business or uploading any information. </p>
+						
+						<h3>Consent</h3>
+						<p class='mb-5'>By using our website, you hereby consent to our disclaimer and agree to its terms.
+						
+						<h3>Update</h3>
+						<p class='mb-5'>Should we update, amend or make any changes to this document, those changes will be prominently posted here</p>
+
+						<p class='mb-5'>If you have any questions about this Privacy Policy, or anything else related to Compario, please <a href='mailto:avinashbarik91@gmail.com'>contact us.</a></p>
+						
+					</div>
+				</div>
+			</div>
+
+			<?php include('footer.inc'); ?>	
+
+	</body>
+</html>
--- public/privacy.php
+++ public/privacy.php
@@ -51,13 +51,13 @@
 						<p class='mb-5'>Compario uses reputable third party vendors to provide traffic analytics, security, and advertising services.</p>
 
 						<h2><u>Our Adevertising Partners</u></h2>
-						<p class='mb-5'>Compario uses reputable third party vendors to provide traffic analytics, security, and advertising services.</p>						
+						<p>Compario uses reputable third party vendors to provide traffic analytics, security, and advertising services.</p>						
 						<p>Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below. Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier access, we hyperlinked to their Privacy Policies below.</p>
 
 						<p>Google</p>
 						<a href='https://policies.google.com/technologies/ads'>https://policies.google.com/technologies/ads</a>
 
-						<p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Compario, which are sent directly to users' browser. They automatically receive your IP address when this occurs. </p><p>These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit. Note that Compario has no access to or control over these cookies that are used by third-party advertiser</p>
+						<p class='mb-5'>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Compario, which are sent directly to users' browser. They automatically receive your IP address when this occurs. </p><p>These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit. Note that Compario has no access to or control over these cookies that are used by third-party advertiser</p>
 
 						<h2><u>Online Privacy Policy Only</u></h2>
 						<p class='mb-5'>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Compario. This policy is not applicable to any information collected offline or via channels other than this website.</p>
